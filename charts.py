948f0ef394a1cc5678cbeba34b0a8efdf11ac3cc
Marvin Menchu <mmenchu@outlook.com>
2023-01-02 19:21:39 -0600

    Se agrego el parámetro name al la función generate bar chart, para guardar el nombre de la imagen con el nombre del país

--- charts.py
+++ charts.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
 
-def generate_bar_chart(labels, values):
+def generate_bar_chart(name, labels, values):
     fig, ax = plt.subplots()
     ax.bar(labels, values)
-    plt.savefig('bar.png')
+    plt.savefig(f'./img/{name}.png')
 
 
 def generate_pie_chart(labels, values):
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def run():
     country = 'Guatemala'
     dict_by_country = utils.dict_by_country(data, country)
     labes, values = utils.population_by_contry(dict_by_country)
-    charts.generate_bar_chart(labes, values)
+    charts.generate_bar_chart(country, labes, values)
     data = utils.population_by_continent(data, 'South America')
     labels, values = utils.world_population(data)
     charts.generate_pie_chart(labels, values)
