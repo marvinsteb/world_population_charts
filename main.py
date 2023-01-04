7f86a6f5776262f4b29d4170fefd11ce7284d37e
Marvin Menchu <mmenchu@outlook.com>
2023-01-03 18:25:37 -0600

    Usando pandas para generar el grafico por continente

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 
 def run():
@@ -11,12 +12,21 @@ def run():
     if bool(dict_by_country):
         labes, values = utils.population_by_contry(dict_by_country[0])
         charts.generate_bar_chart(country, labes, values)
-
-    continent = "South Americaas"
+        
+    continent = "Africa"
+    
+    data_frame = pd.read_csv('./data.csv')
+    data_frame  = data_frame[data_frame['Continent'] == continent]
+    countries = data_frame['Country/Territory'].values
+    percentages = data_frame['World Population Percentage']
+    charts.generate_pie_chart(continent,countries, percentages)
+    
+    '''
     data = utils.population_by_continent(data, continent)
     if bool(data):
         labels, values = utils.world_population(data)
         charts.generate_pie_chart(labels, values)
+    '''
 
 
 if __name__ == "__main__":
